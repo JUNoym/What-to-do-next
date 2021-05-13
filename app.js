ce3653e3945f2e128b7a319a37ab2206885b3820
junoym <junnyaooyama@gmail.com>
2021-05-13 23:48:13 +0900

    🐛 追加ボタンが動かなくなったので修正

--- app.js
+++ app.js
@@ -257,26 +257,5 @@ function recalculate() {
         $(this).children(".probability").first().html(probability + "%");
     });
 }
-$('.add').click(function () {
-    var add = '<tr class="item"><td><div class="color-indicator" style="background-color:#000000;"></div></td><td><input type="text" class="name" value="項目"></td><td><input type="number" class="ratio" value="1"></td><td class="probability"></td><td><button type="button" onclick="rmItem(this)">削除</button></td></tr>';
-    $('#table').append(add);
-    recalculate();
-    if (mode == Mode.waiting) {
-        dataFetch();
-    }
-});
-function rmItem(e) {
-    if ($('.ratio').length > 2) {
-        $(e).parent().parent().remove();
-        recalculate();
-    }
-    if (mode == Mode.waiting) {
-        dataFetch();
-    }
-} code.$
-$('#table').on('change', '.ratio', function () {
-    recalculate();
-    if (mode == Mode.waiting) {
-        dataFetch();
-    }
-});
+
+
